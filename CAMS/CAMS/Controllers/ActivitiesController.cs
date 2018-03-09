3bb581e1a387d1b68d2d11c39b4940065d89bbd3
Azizonen <azizonen@gmail.com>
2018-03-09 21:00:26 +0200

    Connecting Database to the project (Model, Controolers and View)

---
+++ CAMS/CAMS/Controllers/ActivitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CAMS.Models;
+
+namespace CAMS.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        private CAMS_DatabaseEntities db = new CAMS_DatabaseEntities();
+
+        // GET: Activities
+        public ActionResult Index()
+        {
+            var activities = db.Activities.Include(a => a.Computer);
+            return View(activities.ToList());
+        }
+
+        // GET: Activities/Details/5
+        public ActionResult Details(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        // GET: Activities/Create
+        public ActionResult Create()
+        {
+            ViewBag.ComputerId = new SelectList(db.Computers, "ComputerId", "MAC");
+            return View();
+        }
+
+        // POST: Activities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Login,UserName,Logout,Mode,ComputerId")] Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Activities.Add(activity);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ComputerId = new SelectList(db.Computers, "ComputerId", "MAC", activity.ComputerId);
+            return View(activity);
+        }
+
+        // GET: Activities/Edit/5
+        public ActionResult Edit(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ComputerId = new SelectList(db.Computers, "ComputerId", "MAC", activity.ComputerId);
+            return View(activity);
+        }
+
+        // POST: Activities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Login,UserName,Logout,Mode,ComputerId")] Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ComputerId = new SelectList(db.Computers, "ComputerId", "MAC", activity.ComputerId);
+            return View(activity);
+        }
+
+        // GET: Activities/Delete/5
+        public ActionResult Delete(DateTime id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Activity activity = db.Activities.Find(id);
+            if (activity == null)
+            {
+                return HttpNotFound();
+            }
+            return View(activity);
+        }
+
+        // POST: Activities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(DateTime id)
+        {
+            Activity activity = db.Activities.Find(id);
+            db.Activities.Remove(activity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CAMS/CAMS/Controllers/LabsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CAMS.Models;
+
+namespace CAMS.Controllers
+{
+    public class LabsController : Controller
+    {
+        private CAMS_DatabaseEntities db = new CAMS_DatabaseEntities();
+
+        // GET: Labs
+        public ActionResult Index()
+        {
+            var labs = db.Labs.Include(l => l.Department);
+            return View(labs.ToList());
+        }
+
+        // GET: Labs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lab lab = db.Labs.Find(id);
+            if (lab == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lab);
+        }
+
+        // GET: Labs/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName");
+            return View();
+        }
+
+        // POST: Labs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LabId,LabName,Building,RoomNumber,DepartmentId")] Lab lab)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Labs.Add(lab);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName", lab.DepartmentId);
+            return View(lab);
+        }
+
+        // GET: Labs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lab lab = db.Labs.Find(id);
+            if (lab == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName", lab.DepartmentId);
+            return View(lab);
+        }
+
+        // POST: Labs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LabId,LabName,Building,RoomNumber,DepartmentId")] Lab lab)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lab).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Departments, "DepartmentId", "DepartmentName", lab.DepartmentId);
+            return View(lab);
+        }
+
+        // GET: Labs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lab lab = db.Labs.Find(id);
+            if (lab == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lab);
+        }
+
+        // POST: Labs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lab lab = db.Labs.Find(id);
+            db.Labs.Remove(lab);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
