6517c51c74c3ab3fe4eee1e7d6669a8c333af594
Azizonen <azizonen@gmail.com>
2018-04-17 12:02:35 +0300

    .

--- CAMS/CAMS/Scripts/Activities.js
+++ CAMS/CAMS/Scripts/Activities.js
@@ -1,13 +1,4 @@
 ﻿$(document).ready(function () {
-    $(function () {
-        $(".datepicker").datepicker();
-        $('.timepicker').timePicker({
-            timeFormat: 'hh:mm',
-            interval: 60,
-            defaultTime: '11',
-            dynamic: false,
-            dropdown: true,
-            scrollbar: true
-        });
-    });
+    $(".datepicker").datepicker();
+    $(".timepicker").timepicker();
 });
