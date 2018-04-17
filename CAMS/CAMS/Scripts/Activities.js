b5888b8d2fb2dc319c0f8548bde713c48554cecb
Azizonen <azizonen@gmail.com>
2018-04-17 14:15:52 +0300

    labs edit

--- CAMS/CAMS/Scripts/Activities.js
+++ CAMS/CAMS/Scripts/Activities.js
@@ -1,4 +1,8 @@
 ﻿$(document).ready(function () {
-    $(".datepicker").datepicker();
-    $(".timepicker").timepicker();
+    $(".datepicker").datepicker($.datepicker.regional['he']);
+    $(".datepicker").datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
+    $(".timepicker").timepicker({ 'timeFormat': 'H:i', 'step': '60' });
 });
--- CAMS/CAMS/Scripts/Labs.js
+++ CAMS/CAMS/Scripts/Labs.js
@@ -1,16 +1,27 @@
 ﻿$(document).ready(function () {
-    $(".draggable").draggable({
-        containment: "#LabErea",
-        grid: [20, 20]
+        $(".draggable").draggable({
+            containment: "#LabErea",
+            grid: [20, 20]
+        });
     });
-});
 SaveComputersLocations = function () {
+    var coms = {};
     $(".draggable").each(function () {
-            let Computer_Position = $(this).position();
-            let Div_Height = parseFloat($(this).parent().css("height"));
-            let Div_Width = parseFloat($(this).parent().css("width"));
-            let New_Computer_Top = Math.round(Computer_Position.top * 100 / Div_Height)
-            let New_Computer_Left = Math.round(Computer_Position.left * 100 / Div_Width)
-            alert($(this).children(1).text() + '     ' + New_Computer_Top + '%,' + New_Computer_Left + '%');
+        let Computer_Position = $(this).position();
+        let Div_Height = parseFloat($(this).parent().css("height"));
+        let Div_Width = parseFloat($(this).parent().css("width"));
+        let New_Computer_Top = Math.round(Computer_Position.top * 100 / Div_Height);
+        let New_Computer_Left = Math.round(Computer_Position.left * 100 / Div_Width);
+        coms[$(this).children(2).text()] = New_Computer_Top + '%,' + New_Computer_Left + '%';
     });
+
+    $.ajax({
+        url: "/Activities/Update",
+        type: 'post',
+        data: { computers: coms },
+        success: function (data) {
+            alert(data);
+        }
+    });
+
 };
