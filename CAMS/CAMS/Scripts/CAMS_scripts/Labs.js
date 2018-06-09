38b56f8460c4d64368ce01b2a8eb4a5f3df41b25
ollana <olganasirov@gmail.com>
2018-06-09 14:40:11 +0300

    update

--- CAMS/CAMS/Scripts/CAMS_scripts/Labs.js
+++ CAMS/CAMS/Scripts/CAMS_scripts/Labs.js
@@ -11,6 +11,7 @@ Draggable = function () {
 }
 SaveComputersLocations = function () {
     var coms = {};
+    coms[0] = 0;
     $(".multidraggable").each(function () {
         let Computer_Position = $(this).position();
         let Div_Height = parseFloat($(this).parent().css("height"));
@@ -19,6 +20,8 @@ SaveComputersLocations = function () {
         let New_Computer_Left = Math.round(Computer_Position.left * 100 / Div_Width);
         coms[$(this).children(2).text()] = New_Computer_Top + '%,' + New_Computer_Left + '%';
     });
+
+
     $.ajax({
         url: "/Labs/Update",
         type: 'post',
@@ -29,6 +32,7 @@ SaveComputersLocations = function () {
             alert(data);
         }
     });
+
 };
 allowDrop = function (ev) {
     ev.preventDefault();
