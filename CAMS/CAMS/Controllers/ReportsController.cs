b5d15dabda21777e23c5af9374e4b9bb9b3ffc87
ollana <olganasirov@gmail.com>
2018-06-19 12:40:07 +0300

    reports

--- CAMS/CAMS/Controllers/ReportsController.cs
+++ CAMS/CAMS/Controllers/ReportsController.cs
@@ -55,8 +55,8 @@ namespace CAMS.Controllers
                 string title = "מתאריך " + startDate.ToShortDateString() + " עד- " + endDate.Value.ToShortDateString() + inclucdeweekends + "בין השעות " + startHour.Value.ToShortTimeString() + " עד- " + endHour.ToShortTimeString();
                 if (includeAllDay)
                 {
-                    startHour = new DateTime();
-                    endHour = new DateTime().AddHours(-1);
+                    startHour = startDate.Date;
+                    endHour = startDate.Date.AddHours(-1);
                 }
                 if (startHour.Value.Hour >= endHour.Hour)
                     throw new Exception();
